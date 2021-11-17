e9588cd6c36db872bea4b12f2e204a107e95273a
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-11-17 17:11:46 -0600

    Update database.php

--- libs/database.php
+++ libs/database.php
@@ -4,15 +4,15 @@ class Database{
     function connect(){
         try{
         //DATOS PARA CONEXION HEROKU
-       /* $dsn = 'mysql:host=wcwimj6zu5aaddlj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; port=3306; dbname=mmdfc754pwa3llx7;';
+       $dsn = 'mysql:host=wcwimj6zu5aaddlj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; port=3306; dbname=mmdfc754pwa3llx7;';
         $username = 'zfkdp6whp5okxxgy';
-        $password = 'c95r118qp2h6ev0c';*/ 
+        $password = 'c95r118qp2h6ev0c'; 
         
         /*PARA PRUEBAS LOCALES*/
-        $dsn = 'mysql:host=localhost; port=3306; dbname=food;';
+         /*$dsn = 'mysql:host=localhost; port=3306; dbname=food;';
         //El usuario por defecto es ROOT, sin contraseña    
         $username = 'root';
-        $password = '';
+        $password = '';*/
  
         $options = array(
    //La extensión Objetos de Datos de PHP (PDO) 
