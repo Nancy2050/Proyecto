4cbf9501df6387be8509e734c1e892e7a99ee3a0
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-12-19 00:26:54 -0600

    Update menu_gral.php

--- views/menu_gral.php
+++ views/menu_gral.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300i,400,700&display=swap" rel="stylesheet">
     
     <title>MENÚ</title>
-    <link rel="stylesheet" href="<?php echo constant('URL');?>public/CSS/estilos.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/estilos.css" type="text/css">
 
     <div id="header">
     <ul>
