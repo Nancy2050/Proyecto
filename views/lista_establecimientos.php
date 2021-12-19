9cdf676a0df5bf72cc210fe4cd3aad33bb7f9ab8
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-12-19 00:49:14 -0600

    cambio ruta css

--- views/lista_establecimientos.php
+++ views/lista_establecimientos.php
@@ -9,7 +9,7 @@
     <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate"> 
     <meta http-equiv="Pragma" content="no-cache"> 
     <title>LISTA ESTABLECIMIENTOS</title>
-    <link rel="stylesheet" href="<?php echo constant('URL');?>public/CSS/estilos.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/estilos.css" type="text/css">
     <div id="header">
     <ul>
       <li>
