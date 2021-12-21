7fc2c64d4ac64879dfff7ca9489a897049e51155
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-12-20 23:19:10 -0600

    Update home_establecimiento.php

--- views/home_establecimiento.php
+++ views/home_establecimiento.php
@@ -70,7 +70,7 @@ $row=$query->fetch(PDO::FETCH_ASSOC);//devuelve columnas de elementos del establ
         <img src="<?php echo constant('URL');?>public/IMG/redes.png" height="30px" width="30px"/>                               
         REDES SOCIALES</button></a></li>
 
-        <li ><a href="<?php echo constant('URL');?>mesas" id="barra">
+        <li ><a href="#" id="barra">
         <button class="redondo">
         <img src="<?php echo constant('URL');?>public/IMG/mesas.png" height="30px" width="30px"/>                             
         MESAS DISPONIBLES</button></a></li>
@@ -80,7 +80,7 @@ $row=$query->fetch(PDO::FETCH_ASSOC);//devuelve columnas de elementos del establ
         <img src="<?php echo constant('URL');?>public/IMG/carrito.png" height="30px" width="30px"/>                               
         LISTA DE PEDIDOS</button></a></li>
 
-        <li ><a href="<?php echo constant('URL');?>mapa/" id="barra">
+        <li ><a href="<?php echo constant('URL');?>home_cliente" id="barra">
         <button class="redondo" > MAPA</button></a></li>
 
         <li ><a href="<?php echo constant('URL');?>cerrar" id="barra">
